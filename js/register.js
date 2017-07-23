e60558ff0deb374fbb817afbaeea395a1dadebae
qijunjun <2269873327@qq.com>
2017-07-23 23:08:46 +0800

    [更新]注册功能输入框信息验证

--- js/register.js
+++ js/register.js
@@ -6,29 +6,47 @@ $(function(){
     var password = "";
     var confirmPwd = "";
     var email = "";
-    var leibie = "";
-    //封装函数
-    //验证姓名是否为空
-    check(username,"用户名");
-    $("#username").keyup(function(){
-        username = $(this).val();
-        check($(this),"用户名",username);
-    });
+    var userInfo = $(".userInfo");
+    var passwordInfo = $(".passwordInfo");
+    var confirmInfo = $(".confirmInfo");
+    var emailInfo = $(".emailInfo");
+    var reg = /^\w/;
+    //封装函数,验证输入框是否为空
     function check(lei,txt,val){
         if(val == ""){
             $(lei).next().html(txt+"不能为空");
+            return false;
         }else{
             $(lei).next().html("");
         }
     }
+    $("#username").keyup(function(){
+        username = $(this).val();
+        //首先验证用户名是否为空
+        check($(this),"用户名",username);
+        if(!reg.test(username)){
+            $(this).next().html("用户名必须以字母、下划线、数字开头。请检查");
+            return false;
+        }
+    });
     $("#password").keyup(function(){
         password = $(this).val();
         check($(this),"密码",password);
+        //验证密码是否一致
+        if(password != confirmPwd){
+            confirmInfo.html("确认密码和密码不一致！请检查");
+            return false;
+        }
 
     });
     $("#confirmPwd").keyup(function(){
         confirmPwd = $(this).val();
         check($(this),"确认密码",confirmPwd);
+        //验证密码是否一致
+        if(password != confirmPwd){
+            confirmInfo.html("确认密码和密码不一致！请检查");
+            return false;
+        }
 
     });
     $("#email").keyup(function(){
@@ -36,15 +54,19 @@ $(function(){
         check($(this),"邮箱",email);
     });
    $("button").click(function(){
+       //点击按钮提交前验证内容是否为空
        check($("#username"),"用户名",username);
        check($("#password"),"密码",password);
        check($("#confirmPwd"),"确认密码",confirmPwd);
        check($("#email"),"邮箱",email);
-
-       // if(username == ""){
-       //     $("#username").next().html("用户名不能为空");
-       // }else{
-       //     $("#username").next().html("");
-       // }
+       if(!reg.test(username)){
+           userInfo.append(" 用户名必须以字母、下划线、数字开头。请检查");
+           return false;
+       }
+       //验证密码是否一致
+       if(password != confirmPwd){
+           confirmInfo.html("确认密码和密码不一致！请检查");
+           return false;
+       }
    }) 
 });
